822b1e99015e16da8f20c9bd2951c8fe81c35c07
Benjamin Motz <benjamin.motz@sinclair.edu>
2025-02-22 13:47:01 -0500

    Add inputBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./components/home";
 // import Profile from "./components/ai/profile";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
