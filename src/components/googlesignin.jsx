2a9a242badc5f87acc93c5c8ec521bd5c14deb5d
Benjamin Motz <benjamin.motz@sinclair.edu>
2025-02-22 17:49:14 -0500

    profile page

--- src/components/googlesignin.jsx
+++ src/components/googlesignin.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { auth, provider, signInWithPopup } from '../utils/firebase';
 
 const GoogleSignIn = () => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
     try {
@@ -16,13 +18,17 @@ const GoogleSignIn = () => {
     }
   };
 
+  const handleProfileClick = () => {
+      navigate('/profile');
+    }
+  };
+
   return (
     <div>
       {user ? (
         <div>
-          <h2>Welcome, {user.displayName}!</h2>
-          <img src={user.photoURL} alt={user.displayName} />
-          <p>Email: {user.email}</p>
+          <h3>Welcome, {user.displayName}!</h3>
+          <img src={user.photoURL} alt={user.displayName} onClick={handleProfileClick} style={{ cursor: 'pointer' }} />
         </div>
       ) : (
         <button onClick={handleGoogleSignIn}>Sign in with Google</button>
